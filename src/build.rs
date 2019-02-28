9fe1db0c83dff5365b8571b50a4d12d3ff9d3013
Elichai Turkel <elichai@enigma.co>
2019-02-28 16:52:47 +0200

    Increased the module memory to 128

--- src/build.rs
+++ src/build.rs
@@ -80,10 +80,10 @@ pub fn build(
 			module = new_module;
 			let mut stack_top_page = new_stack_top / 65536;
 			if new_stack_top % 65536 > 0 { stack_top_page += 1 };
-			module = externalize_mem(module, Some(stack_top_page), 62);
+			module = externalize_mem(module, Some(stack_top_page), 128);
 		} else {
 			// Temp change for enlarging default number of pages for memory size
-			module = externalize_mem(module, Some(60)/*None*/, 62);
+			module = externalize_mem(module, Some(120)/*None*/, 128);
 		}
 	}
 
